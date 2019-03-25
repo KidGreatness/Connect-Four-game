991a02273ea53c9154f60ca0f36e036fec64b720
Benjamin Bryant <kidgreatness@gmail.com>
2019-03-25 15:47:26 -0400

    game mechanics finished and UI updates

--- connectfour.js
+++ connectfour.js
@@ -1,8 +1,6 @@
 // Player Variables 
 var playerOne,playerTwo;
-var playerOneChip = 'player-one-chip';
-var playerTwoChip = 'player-two-chip';
-var playerOneTurn = false;
+var playerOneTurn = true;
 var playerTwoTurn = false;
 var playerOneWins = 0;
 var playerTwoWins = 0;
@@ -10,24 +8,60 @@ var playerOneLosses = 0;
 var playerTwoLosses = 0;
 var playerOneTurnCount = 0;
 var playerTwoTurnCount = 0;
-var lastChipLocation = 0;
 var playerOneMatchingChipsHor = 0;
 var playerTwoMatchingChipsHor = 0;
 var playerOneMatchingChipsVer = 0;
 var playerTwoMatchingChipsVer = 0;
 var gameEnded;
 
+var playerOneChipClass = 'player-one-chip';
+var playerTwoChipClass = 'player-two-chip';
+var playerOneWinBGClass = 'player-one-win-bg';
+var playerTwoWinBGClass = 'player-two-win-bg';
+
+var playerTwoBackgroundClass = 'player-two-background';
+var playerTwoTopRowClass = 'player-two-top-row';
+var highlightedClass ='highlighted';
+
+var defaultBoardClassese = 'board-holder player-one-background';
+var defaultPlayerHubClasses = '';
 
 var chips = document.getElementsByClassName('chip');
-var newGameButton = document.getElementById('new-game-button');
-var playerOneWinsID = document.getElementById('player-one-wins');
-var playerTwoWinsID = document.getElementById('player-one-losses');
-var playerOneLossesID = document.getElementById('player-two-wins');
-var playerTwoLossesID = document.getElementById('player-two-losses');
+var lastChipLocation = chips.length - 1;
 
-newGameButton.addEventListener('click', newGame);
+var DOMstrings = {
+	boardHolder: document.querySelector('.board-holder'),
+	board: document.querySelector('.board'),
+	topRow: document.querySelector('.holes-row6'),
+	playerOneTitle: document.querySelector('.player-one-hub h2'),
+	playerTwoTitle: document.querySelector('.player-two-hub h2'),
+	newGameButton: document.getElementById('new-game-button'),
+	playerOneWinsID: document.getElementById('player-one-wins'),
+	playerTwoWinsID: document.getElementById('player-one-losses'),
+	playerOneLossesID: document.getElementById('player-two-wins'),
+	playerTwoLossesID: document.getElementById('player-two-losses')
+};
+
+DOMstrings.newGameButton.addEventListener('click', newGame);
+DOMstrings.board.addEventListener('click', function(event) {
+	var holeClicked;
+
+	holeClicked = parseInt(event.target.classList[1]);
+	
+	if (gameEnded !== true) {
+			
+			if (Number.isInteger(holeClicked)) {
+				PlayerTurn();
+				placeMoveInGrid(selectColumns(holeClicked));
+				winClause();
+				winClauseDiagonal();
+			}	
+	}		
+});
+
 var grid = {};
-var columnBottomsArr = [48,47,46,45,44,43,42]
+var columnTopArr = [6,5,4,3,2,1,0];
+var columnBottomsArr = [41,40,39,38,37,36,35];
 
 var Hole = function (hole, chip, classListNumber, columnID) {
 	this.hole = hole;
@@ -39,35 +73,25 @@ var Hole = function (hole, chip, classListNumber, columnID) {
 }
 
 for(var x = 0; x < 7; x++) {
-	for (var i = 48; i >= 7; i = i - 7) {
+	for (var i = lastChipLocation; i >= 0; i = i - 7) {
 		var currentHoleStr = String(i - x);
 
 		var	currentHoleObj = new Hole (Number(currentHoleStr),chips[i - x], i - x, x);
-		currentHoleObj.chip.addEventListener('click',  function () {
-			if (gameEnded !== true) {
-				PlayerTurn();
-				placeMoveInGrid(selectColumns(this.classList[1]));
-				winClause()
-				winClauseDiagonal()
-			}
-		});
 
 		grid[currentHoleStr] = currentHoleObj;
 	}
 }
 
-
-
 function selectColumns(hole) {
 	var currentHoleClicked = grid[hole];
 	var lastMove = columnBottomsArr[currentHoleClicked.columnID];
 
-	if (columnBottomsArr[currentHoleClicked.columnID] >= 7) {
+	if (columnBottomsArr[currentHoleClicked.columnID] >= 0) {
 		if (playerOneTurn == true) {
-				chips[columnBottomsArr[currentHoleClicked.columnID]].classList.add('player-one-chip');
+				chips[columnBottomsArr[currentHoleClicked.columnID]].classList.add(playerOneChipClass);
 		}
 		else {
-			chips[columnBottomsArr[currentHoleClicked.columnID]].classList.add('player-two-chip');	
+			chips[columnBottomsArr[currentHoleClicked.columnID]].classList.add(playerTwoChipClass);	
 		}
 
 		columnBottomsArr[currentHoleClicked.columnID] = columnBottomsArr[currentHoleClicked.columnID] - 7;
@@ -82,17 +106,19 @@ function PlayerTurn() {
 	if (playerOneTurnCount == playerTwoTurnCount) {
 		playerOneTurn = true;
 		playerTwoTurn = false;
-		document.querySelector(".board-holder").classList.add('player-two-background');
-		document.querySelector(".player-one-hub h2").style.fontWeight = "normal";
-		document.querySelector(".player-two-hub h2").style.fontWeight = "bold";
+		DOMstrings.boardHolder.classList.toggle(playerTwoBackgroundClass);
+		DOMstrings.topRow.classList.toggle(playerTwoTopRowClass);
+		DOMstrings.playerOneTitle.classList.toggle(highlightedClass);
+		DOMstrings.playerTwoTitle.classList.toggle(highlightedClass);
 		playerOneTurnCount ++;
 	}
 	else {
 		playerTwoTurn = true;
 		playerOneTurn = false;
-		document.querySelector(".board-holder").classList.remove('player-two-background');
-		document.querySelector(".player-two-hub h2").style.fontWeight = "normal";
-		document.querySelector(".player-one-hub h2").style.fontWeight = "bold";
+		DOMstrings.boardHolder.classList.toggle(playerTwoBackgroundClass);
+		DOMstrings.topRow.classList.toggle(playerTwoTopRowClass);
+		DOMstrings.playerOneTitle.classList.toggle(highlightedClass);
+		DOMstrings.playerTwoTitle.classList.toggle(highlightedClass);
 		playerTwoTurnCount ++;
 	}
 }
@@ -100,7 +126,7 @@ function PlayerTurn() {
 function currentBoardState() {
 
 	for(var x = 0; x < 7; x++) {
-		for (var i = 48; i >= 7; i = i - 7) {
+		for (var i = lastChipLocation; i >= 0; i = i - 7) {
 			if(grid[i - x].playerChipInHole == 1) {
 				playerOneMatchingChipsVer++;
 				console.log('player 1: ' + playerOneMatchingChipsVer);
@@ -122,7 +148,7 @@ function winClause() {
 	var playerTwoMatches = [ [], [] ];
 	//Checking vertical matches
 	for(var x = 0; x < 7; x++) {
-		for (var i = 48; i >= 7; i = i - 7) {
+		for (var i = lastChipLocation; i >= 0; i = i - 7) {
 			if(grid[i - x].playerChipInHole == 1) {
 				playerOneMatches[0].push(i - x);
 			} else if(grid[i - x].playerChipInHole == 2) {
@@ -133,7 +159,7 @@ function winClause() {
 	}
 	//Checking horizontal matches
 	for (var x = 0; x < 42; x = x + 7) {
-		for (var i = 48; i > 41; i--) {
+		for (var i = lastChipLocation; i > 34; i--) {
 			if(grid[i - x].playerChipInHole == 1) {
 				playerOneMatches[1].push(i - x);
 			} else if(grid[i - x].playerChipInHole == 2) {
@@ -141,7 +167,6 @@ function winClause() {
 			}	
 		}
 	}
-
 	verifyAdjacent(playerOneMatches,playerTwoMatches);
 }
 
@@ -172,13 +197,13 @@ function verifyAdjacent(playerOneMatches,playerTwoMatches) {
 					break;
 
 				case 1:
-					if(playerOneAdjacentHoles == 1){
+					if(playerOneAdjacentHoles == 1 && playerOneMatches[x][y] % 7 !== 0){
 						playerOneNumOfAdjacentHoles--;
 						if (playerOneNumOfAdjacentHoles === 0) return gameOver();
 					} else {
 						playerOneNumOfAdjacentHoles = 3;
 					}
-					if(playerTwoAdjacentHoles == 1){
+					if(playerTwoAdjacentHoles == 1 && playerTwoMatches[x][y] % 7 !== 0){
 						playerTwoNumOfAdjacentHoles--;
 						if (playerTwoNumOfAdjacentHoles === 0) return gameOver();
 					} else {
@@ -197,7 +222,7 @@ function verifyAdjacent(playerOneMatches,playerTwoMatches) {
 }
 
 function placeMoveInGrid(hole) {
-	if( hole >= 7){
+	if( hole >= 0){
 		if (playerOneTurn == true) {
 				grid[hole].playerChipInHole =  1;
 			}
@@ -222,34 +247,34 @@ function winClauseDiagonal() {
 	}
 	
 	for(var x = 0; x < 6; x++) {
-		switch (x){
+		switch (x) {
 			case 0:
-				for(var i = 21; i < 46; i = i + 8) {
+				for(var i = 14; i < 39; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
 			case 1:	
-				for(var i = 14; i < 47; i = i + 8) {
+				for(var i = 7; i < 40; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
 			case 2:
-				for(var i = 7; i < 48; i = i + 8) {
+				for(var i = 0; i < 41; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
 			case 3:
-				for(var i = 8; i < 49; i = i + 8) {
+				for(var i = 1; i < 42; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
 			case 4:
-				for(var i = 9; i < 42; i = i + 8) {
+				for(var i = 2; i < 35; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
 			case 5:
-				for(var i = 10; i < 35; i = i + 8) {
+				for(var i = 10; i < 28; i = i + 8) {
 					addHoletoArr(playerOneBackDiagonalArr, playerTwoBackDiagonalArr);
 				}
 				break;
@@ -257,34 +282,34 @@ function winClauseDiagonal() {
 	}
 
 	for(var x = 0; x < 6; x++) {
-		switch (x){
+		switch (x) {
 			case 0:
-				for(var i = 27; i < 46; i = i + 6) {
+				for(var i = 20; i < 39; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
 			case 1:	
-				for(var i = 20; i < 45; i = i + 6) {
+				for(var i = 13; i < 38; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
 			case 2:
-				for(var i = 13; i < 44; i = i + 6) {
+				for(var i = 6; i < 37; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
 			case 3:
-				for(var i = 12; i < 43; i = i + 6) {
+				for(var i = 5; i < 36; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
 			case 4:
-				for(var i = 11; i < 36; i = i + 6) {
+				for(var i = 4; i < 29; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
 			case 5:
-				for(var i = 10; i < 29; i = i + 6) {
+				for(var i = 3; i < 22; i = i + 6) {
 					addHoletoArr(playerOneForwardDiagonalArr, playerTwoForwardDiagonalArr);
 				}
 				break;
@@ -346,31 +371,34 @@ function gameOver() {
 	gameEnded = true;
 
 	if (playerOneTurn === true) {
-		playerOneWinsID.textContent = ++playerOneWins;
-		playerTwoLossesID.textContent = ++playerTwoLosses;
-		document.querySelector(".board-holder").classList.add('player-one-win-bg');
+		DOMstrings.playerOneWinsID.textContent = ++playerOneWins;
+		DOMstrings.playerTwoLossesID.textContent = ++playerTwoLosses;
+		DOMstrings.boardHolder.classList.add(playerOneWinBGClass);
 	} else if (playerTwoTurn === true) {
-		playerTwoWinsID.textContent = ++playerTwoWins;
-		playerOneLossesID.textContent = ++playerOneLosses;
-		document.querySelector(".board-holder").classList.add('player-two-win-bg');
+		DOMstrings.playerTwoWinsID.textContent = ++playerTwoWins;
+		DOMstrings.playerOneLossesID.textContent = ++playerOneLosses;
+		DOMstrings.boardHolder.classList.add(playerTwoWinBGClass);
 	}
 }
 
 function newGame() {
-	columnBottomsArr = [48,47,46,45,44,43,42]
+	columnBottomsArr = [41,40,39,38,37,36,35];
 	playerOneTurn = false;
 	playerTwoTurn = false;
 	playerOneTurnCount = 0;
 	playerTwoTurnCount = 0;
 	gameEnded = false;
 
-	document.querySelector(".board-holder").classList.value = "board-holder player-one-background";
-	document.querySelector(".player-one-hub h2").style.fontWeight = "bold";
+	DOMstrings.boardHolder.classList.value = defaultBoardClassese;
+	DOMstrings.playerOneTitle.classList.value = highlightedClass;
+	DOMstrings.playerTwoTitle.classList.value = '';
+	DOMstrings.topRow.classList.remove(playerTwoTopRowClass);
 	
 	for(var x = 0; x < 7; x++) {
-		for (var i = 48; i >= 7; i = i - 7) {
-			chips[i - x].classList.value = 'chip ' + (i -x);
+		for (var i = lastChipLocation; i >= 0; i = i - 7) {
+			chips[i - x].classList.value = 'chip ' + (i - x);
 			grid[i - x].playerChipInHole = 0;
 		}
 	}
 }
+
